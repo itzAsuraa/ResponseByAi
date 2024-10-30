e9b9cbd35fa8c2c452517d9c4527f8c263b1ba67
⏤͟͟͞͞ 🇮🇳 𝐓ʜᴇ 𝐂ᴀᴘᴛᴀɪɴ's / <167273277+itzAsuraa@users.noreply.github.com>
2024-10-30 19:15:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,12 +1,10 @@
 import re
 from os import environ, getenv
 
-
 API_ID = environ.get("API_ID", "")
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 OWNER_ID = int(environ.get("OWNER_ID", ""))
 MONGO_URL = environ.get("MONGO_URL", "")
-LOG_GROUP = environ.get("LOG_GROUP", "-1002100219353")
 AUTH_CHANNEL = int(environ.get("AUTH_CHANNEL", ""))
 FSUB = environ.get("FSUB", True)
