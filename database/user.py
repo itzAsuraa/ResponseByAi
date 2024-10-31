66b9537b959fe195b7a3891e60141a14da16061b
⏤͟͟͞͞ 🇮🇳 𝐓ʜᴇ 𝐂ᴀᴘᴛᴀɪɴ's / <167273277+itzAsuraa@users.noreply.github.com>
2024-10-31 16:15:35 +0530

    fix issue

--- database/user.py
+++ database/user.py
@@ -2,7 +2,16 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from info import MONGO_URL
 client = AsyncIOMotorClient(MONGO_URL)
 db = client["ResponseByAi"]
+userList = db.userList
 
 async def total_users():
-    count = await userList.count_documents({})
+    count = await UserList.count_documents({})
     return count
+
+def addUser(userId , userName):
+    dets = {
+        'userId': userId,
+        'userName': userName
+    }
+    userList.insert_one(dets).inserted_id
+    return
