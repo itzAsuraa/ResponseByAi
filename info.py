4c1690fd38a6884c966c893cb3384104416ca5b2
⏤͟͟͞͞ 🇮🇳 𝐓ʜᴇ 𝐂ᴀᴘᴛᴀɪɴ's / <167273277+itzAsuraa@users.noreply.github.com>
2024-11-28 16:20:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,7 +7,7 @@ from os import getenv
 API_ID = getenv("API_ID", "")
 API_HASH = getenv("API_HASH", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
+OWNER_ID = int(getenv("OWNER_ID", 7453278496))
 MONGO_URL = getenv("MONGO_URL", "")
 AUTH_CHANNEL = int(getenv("AUTH_CHANNEL", ""))
 FSUB = getenv("FSUB", True)
